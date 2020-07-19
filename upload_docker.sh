ad947f66d37643e960b46d08a7bed0647a7f75be
hemanthreddy44 <65831040+hemanthreddy44@users.noreply.github.com>
2020-07-20 03:17:41 +0530

    update

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+##!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
@@ -6,14 +6,17 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath="hemanthhr/flasksklearn"
+dockerpath=hemanthhr/flask
+
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-docker login 
-&&\
-    docker image tag flasksklearn $dockerpath
+export DOCKER_ID_USER="hemanthhr"
+docker login
+docker tag flask $DOCKER_ID_USER/flask
+docker push $DOCKER_ID_USER/flask
 
 # Step 3:
-# Push Image
-docker push hemanthhr/container:flasklearn
+# Push image to a docker repository
+docker push hemanthhr/flask
+
