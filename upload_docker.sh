c47d052c6e5da28ddce0ed168e5dec9986edaad5
hemanthreddy44 <65831040+hemanthreddy44@users.noreply.github.com>
2020-07-20 00:58:28 +0530

    docker output updated

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath="hemanthhr/flasksklearn"
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login 
+&&\
+    docker image tag flasksklearn $dockerpath
 
 # Step 3:
-# Push image to a docker repository
+# Push Image
+docker push hemanthhr/container:flasklearn
